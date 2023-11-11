942cd33dcd19c67dcdecd2466fa4891b8cae8d03
Etienne <etienne1995@orange.fr>
2023-11-11 14:54:17 +0100

    init project

---
+++ apps/ha-matter-bridge/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'ha-matter-bridge',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/ha-matter-bridge',
+};
